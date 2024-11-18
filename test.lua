a157db894d5d417f41d10a04d7c76ae3492bdd4b
Arcturus917 <151203395+Arcturus917@users.noreply.github.com>
2024-11-17 23:29:08 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -202,7 +202,7 @@ end
 
 --[[ end configs, don't edit this especially if you have no idea what Lua is lmao ]]--
 
-local lunarbotversion = "v0.1.3 Tester Only Release (made by mrdodoman27)"
+local lunarbotversion = "v0.1.3 Beta Release"
 local lunarbotchangelogs = "Added a few commands!"
 
 local gameData = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId)
@@ -230,7 +230,7 @@ local function chat(msg)
 end
 
 local funfacts = {
-	"I was created was created on 11/17/22 by MrDodoMan27.",
+	"I was created was created on 11/17/22.",
 	"I added this for no reason",
 	"I made this for no reason",
 	"My dad came back from getting the milk 0.03 seconds ago.",
@@ -238,13 +238,13 @@ local funfacts = {
 	"If you spend a penny, you lose that penny.",
 	"Press 'Y' to jump.",
 	"Commands are added to LunarBot almost every day.",
-	"I was developed and scripted by MrDodoMan.",
+	"I was developed and scripted.",
 	"Im is a self-bot, meaning that, yes, I AM A REAL PERSON. I'm watching... :)",
 	"Among Us is extremely old.",
 	"Press Alt + F4 to get 1 billion dollars on the spot.",
 	"Press U to walk forward.",
 	"At the time of writing this fun fact list, there are people in the server.",
-	"MrDodoMan was first tested in the game: a literal baseplate.",
+	"I was first tested in the game: a literal baseplate.",
 	"I found a lucky penny!",
 	"You found this fun fact.",
 	"The sandwich was invented in the 1700s.",
@@ -390,7 +390,7 @@ commands = {
 	reset = {
 		Name = "reset",
 		Aliases = {"re"},
-		Use = "Respawns MrDodoMan!",
+		Use = "Respawns Me!",
 		Enabled = true,
 		CommandFunction = function(msg, args, speaker)
 			local hum = bot.Character:FindFirstChildWhichIsA("Humanoid")
@@ -410,7 +410,7 @@ commands = {
 		
 			if #game.Players:GetPlayers() <= 1 then
 				print("Rejoining (NEW SERVER)")
-				game.Players.LocalPlayer:Kick("\nMrDodoMan - Rejoining...")
+				game.Players.LocalPlayer:Kick("\nMe - Rejoining...")
 				wait()
 				game:GetService('TeleportService'):Teleport(game.PlaceId, game.Players.LocalPlayer)
 			else
@@ -448,14 +448,14 @@ commands = {
 			
 			if plr then
 				bot.Character:SetPrimaryPartCFrame(CFrame.new(plr.Character.HumanoidRootPart.Position))
-				chat("📸 CAUGHT IN 4K BY MRDODOMAN AND MOON CARETAKER 📸")
+				chat("📸 CAUGHT IN 4K BY MRDODOMAN 📸")
 			end
 		end,
 	},
 	ping = {
 		Name = "ping",
 		Aliases = {"getping"},
-		Use = "Chats MrDodoMans's ping!",
+		Use = "Chats my ping!",
 		Enabled = true,
 		CommandFunction = function(msg, args, speaker)
 			chat("Ping: " .. tostring(math.floor(game:GetService("Stats").PerformanceStats.Ping:GetValue() + 0.5)) .. " ms")
@@ -464,7 +464,7 @@ commands = {
 	executor = {
 		Name = "executor",
 		Aliases = {"identifyexecutor", "getexec", "exec"},
-		Use = "Gives you the executor that is running MrDodoMan!",
+		Use = "Gives you the executor that is running Me!",
 		Enabled = true,
 		CommandFunction = function(msg, args, speaker)
 			chat("Executor: " .. identifyexecutor() or "Unknown")
@@ -473,11 +473,11 @@ commands = {
 	lua = {
 		Name = "lua",
 		Aliases = {"runlua", "run", "luau"},
-		Use = "Gives you the executor that is running MrDodoMan!",
+		Use = "Gives you the executor that is running Me!",
 		Enabled = true,
 		CommandFunction = function(msg, args, speaker)
 			if speaker ~= bot.Name then
-				chat("You do not have permission to run LuaU from MrDodoMan.")
+				chat("You do not have permission to run LuaU from Me.")
 				return
 			end
 			
@@ -1453,15 +1453,15 @@ if req ~= nil then
 end
 
 task.spawn(function()
-	chat("MrDodoMan " .. lunarbotversion .. " Loaded in " .. os.time() - bootTime .. " seconds!")
+	chat("Unnamed " .. lunarbotversion .. " Loaded in " .. os.time() - bootTime .. " seconds!")
 	wait(0.1)
-	chat("You can now control this client! Type " .. prefix .. "cmds to view commands.")
+	chat("People that are whitelisted can now control this client! Type " .. prefix .. "cmds to view commands.")
 end)
 
 task.spawn(function()
-	while wait(300) do
+	while wait(1000000000000000000000000000000000000000000000000000000000000000000000000000) do
 		if disconnected == false then
-			chat("MrDodoMan is currently active! Type " .. prefix .. "cmds to view commands.")
+			chat("Im currently active! Type " .. prefix .. "cmds to view commands.")
 		end
 	end
 end)
